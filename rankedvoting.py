c2e4b52c50d918ae84924f5808e61d3e8a229746
davidtweddell <david.tweddell@gmail.com>
2020-07-15 07:25:15 -0400

    implemented all three methods

--- rankedvoting.py
+++ rankedvoting.py
@@ -101,28 +101,17 @@ def main():
     # where are we now?
     frame = inspect.currentframe().f_code.co_name
 
-    # # CFI-EOF
-    # # load the file
-    # fn = "./covid-nois.xlsx"
-    # df = read_ranked_columns(fn)
-    # # list of candidates
-    # candidates, candidate_names = make_candidate_list(df)
-    # # list of voters
-    # voters_list = ['voter1', 'voter2', 'voter3']
-    # # make ballots
-    # ballots = make_ballots(df, candidate_names, voters_list)
-
-
-
     # TD Ready - totally different data structure
     df2 = read_forms_data("./td_ready.xlsx").values.tolist()
     ballots = []
     candidates = []
 
     # make the ballots
+    utils.print_message(frame, "Making ballots.")
+
     for item in df2:
+        # ranked list is in column 5 (with 0-based index)
         b = item[5].split(';')
-        print(b)
         # drop the last empty item
         # probably other ways to do this to, with an iterator maybe
         b = b[:(len(b)-1)]
@@ -131,27 +120,54 @@ def main():
         print(ballots)
 
     # make the list of candidates
-    c = sorted(df2[1][5].split(';')[:5])
-    for the_candidate in c:
+    utils.print_message(frame, "Making list of candidate names.")
+
+    # use the last ballot, sorted, to get the list of candidate names
+    for the_candidate in sorted(b):
         candidates.append(Candidate(the_candidate))
     # show the list of Candidate objects
     if VERBOSE:
         print(candidates)
 
 
-    # run the vote
+    # run the STV vote
     utils.print_message(frame, "Executing STV selection.")
-    election_result = pyrankvote.single_transferable_vote(
+    stv_election_result = pyrankvote.single_transferable_vote(
         candidates, 
         ballots, 
         number_of_seats=1
     )
+    stv_winner = stv_election_result.get_winners()[0].name
+    utils.print_message(frame, "STV Selected: {0}".format(stv_winner))
+    if VERBOSE:
+        utils.print_message(frame, "STV Selection Result.")
+        print(stv_election_result)
 
-    # Show the evolution and the final result
-    utils.print_message(frame, "STV Selection Result.")
-    print(election_result)
+
+    # run the PBV vote
+    utils.print_message(frame, "Executing PBV selection.")
+    pbv_election_result = pyrankvote.preferential_block_voting(
+        candidates, 
+        ballots, 
+        number_of_seats=1
+    )
+    pbv_winner = pbv_election_result.get_winners()[0].name
+    utils.print_message(frame, "PBV Selected: {0}".format(pbv_winner))
+    if VERBOSE:
+        utils.print_message(frame, "PBV Selection Result.") 
+        print(pbv_election_result)
 
 
+    # run the IRV vote
+    utils.print_message(frame, "Executing IRV selection.")
+    irv_election_result = pyrankvote.instant_runoff_voting(
+        candidates, 
+        ballots)
+    irv_winner = irv_election_result.get_winners()[0].name
+    utils.print_message(frame, "IRV Selected: {0}".format(irv_winner))
+    if VERBOSE:
+        utils.print_message(frame, "IRV Selection Result.")
+        print(pbv_election_result)
 
 
 if __name__ == "__main__":
